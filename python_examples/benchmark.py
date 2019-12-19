78e198ab20b49b48c2624a8fb1bb7684e03ed9eb
Ryan <rwickman@memphis.edu>
2019-12-19 16:34:54 -0600

    Add linear regression to resdiual diagnostics

--- python_examples/benchmark.py
+++ python_examples/benchmark.py
@@ -4,16 +4,17 @@ from CryptoPrediction import CryptoPrediction
 import pandas as pd
 import numpy as np
 from scipy import stats, special
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
 
-btc_df = pd.read_csv("../data/gemini_BTCUSD_2019_1min.csv")
+btc_df = pd.read_csv("../data/Gemini_BTCUSD_d.csv")
 
 cp = CryptoPrediction()
 data = btc_df["Close"][::-1].to_numpy()
-# data, maxlog = stats.boxcox(data)
+data, maxlog = stats.boxcox(data)
 X, y = cp.create_examples(data, 14)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=False)
 
 y_pred_naive = []
 for i in range(len(X)):
@@ -42,6 +43,15 @@ for i in range(len(X)):
 lin_reg = LinearRegression().fit(X_train, y_train)
 y_pred_lin_reg = lin_reg.predict(X_test)
 
+X_train = sm.add_constant(X_train)
+X_test = sm.add_constant(X_test)
+model = sm.OLS(y_train, X_train)
+results  = model .fit()
+print(results.summary())
+
+lasso_reg = Lasso(alpha=0.1).fit(X_train, y_train)
+y_pred_lasso_reg = lasso_reg.predict(X_test) 
+
 # inverse_boxcox = lambda el : special.inv_boxcox(el, maxlog)
 # y = np.array(list(map(inverse_boxcox, y)))
 
@@ -59,3 +69,4 @@ print("AVERAGE MSE (14)", cp.mse(y_pred_avg_14, y))
 print("DRIFT (all data) MSE: ", cp.mse(y_pred_drift, y))
 print("DRIFT Partial MSE: ", cp.mse(y_pred_drift_partial, y))
 print("Linear Regression MSE: ", cp.mse(y_pred_lin_reg, y_test))
+print("Lasso Regression MSE: ", cp.mse(y_pred_lasso_reg, y_test))
--- python_examples/residual_diagonstics.py
+++ python_examples/residual_diagonstics.py
@@ -5,13 +5,16 @@ import pandas as pd
 import numpy as np
 import statsmodels as sm
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 #.stats.diagnostic.acorr_ljungbox
 
 cp = CryptoPrediction()
-btc_df = pd.read_csv("../data/Gemini_BTCUSD_d.csv")
+btc_df = pd.read_csv("../data/gemini_BTCUSD_2019_1min.csv")
 data = btc_df["Close"][::-1].to_numpy()
 lags = 14
 X, y = cp.create_examples(data, lags)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, shuffle=False)
 
 y_pred_naive = []
 for i in range(len(X)):
@@ -21,17 +24,22 @@ y_pred_avg_3 = []
 for i in range(len(X)):
     y_pred_avg_3.append(cp.avg_forecast(X[i], 3))
 
-residuals = np.subtract(y, np.array(y_pred_avg_3))
+
+lin_reg = LinearRegression().fit(X_train, y_train)
+y_pred_lin_reg = lin_reg.predict(X_test)
+
+residuals = np.subtract(y_test, np.array(y_pred_lin_reg))
 res_df = pd.DataFrame(residuals)
 print(res_df.describe())
 print("NAIVE MSE: ", cp.mse(np.array(y_pred_naive), y))
 print("AVERAGE MSE (3)", cp.mse(np.array(y_pred_avg_3), y))
+print("Linear Regression MSE: ", cp.mse(y_pred_lin_reg, y_test))
 # res_df.plot()
 # res_df.hist()
 # res_df.plot(kind='kde')
 # sm.graphics.gofplots.qqplot(res_df)
 
-sm.graphics.tsaplots.plot_acf(res_df, lags=50)
+sm.graphics.tsaplots.plot_acf(res_df, lags=lags)
 #lbvalue, pvalue = sm.stats.diagnostic.acorr_ljungbox(res_df)
 #plt.plot(pvalue[1:])
 
